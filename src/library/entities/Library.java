e4c44b4f363be98f3d1fbb5811b37d059d1fc47b
Utkrist Shrestha <87806021+utkriststha@users.noreply.github.com>
2021-08-15 17:40:27 +1000

    Correction till Line 109
    I made mistake correcting the constant variable name.
    Constant variable name, Variable name and Method name till line 109 is corrected

--- src/library/entities/Library.java
+++ src/library/entities/Library.java
@@ -17,12 +17,12 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public class Library implements Serializable {
 	
-	private static final String libraryFile = "library.obj"; //Changed 'lIbRaRyFiLe' to 'self'
-	private static final int loanLimit = 2; //Changed 'lOaNlImIt' to 'loanLimit'
-	private static final int loanPeriod = 2;
-	private static final double finePerDay = 1.0; //Changed 'FiNe_PeR_DaY' to 'FinePerDay'
-	private static final double maxFinesOwed = 1.0;
-	private static final double damageFee = 2.0;
+	private static final String LIBRARY_FILE = "library.obj"; //Changed 'lIbRaRyFiLe' to 'LIBRARY_FILE'
+	private static final int LOAN_LIMIT = 2; //Changed 'lOaNlImIt' to 'LOAN_LIMIT'
+	private static final int LOAN_PERIOD = 2; //Changed 'loanPeriod' to 'LOAN_PERIOD'
+	private static final double FINE_PER_DAY = 1.0; //Changed 'FiNe_PeR_DaY' to 'FINE_PER_DAY'
+	private static final double MAX_FINES_OWED = 1.0; //Changed 'maxFinesOwed' to 'MAX_FINES_OWED'
+	private static final double DAMAGE_FEE = 2.0; //Changed 'damageFee' to 'DAMAGE_FEE'
 	
 	private static Library self; //Changed 'SeLf' to 'self'
 	private int bookId; //Changed 'bOoK_Id' to 'bookId'
@@ -30,52 +30,52 @@ public class Library implements Serializable {
 	private int loanid; //Changed 'lOaN_Id;' to 'loadId'
 	private Date loanDate; //Changed 'lOaN_DaTe' to 'loanDate'
 	
-	private Map<Integer, Book> Catalog; //Changed 'CaTaLoG' to 'Catalog' 
-	private Map<Integer, Member> Members; //Changed 'MeMbErS' to 'Members'
-	private Map<Integer, Loan> Loans; //Changed 'LoAnS' to 'Loans'
-	private Map<Integer, Loan> CurentLoans; //Changed 'CuRrEnT_LoAnS' to 'CurrentLoans'
-	private Map<Integer, Book> DamagedBooks; //Changed 'DaMaGeD_BoOkS' to 'DamagedBooks'
+	private Map<Integer, Book> catalog; //Changed 'CaTaLoG' to 'catalog' 
+	private Map<Integer, Member> members; //Changed 'MeMbErS' to 'members'
+	private Map<Integer, Loan> loans; //Changed 'LoAnS' to 'loans'
+	private Map<Integer, Loan> curentLoans; //Changed 'CuRrEnT_LoAnS' to 'currentLoans'
+	private Map<Integer, Book> damagedBooks; //Changed 'DaMaGeD_BoOkS' to 'damagedBooks'
 	
 
 	private Library() {
-		CaTaLoG = new HashMap<>();
-		MeMbErS = new HashMap<>();
-		LoAnS = new HashMap<>();
-		CuRrEnT_LoAnS = new HashMap<>();
-		DaMaGeD_BoOkS = new HashMap<>();
-		bOoK_Id = 1;
-		mEmBeR_Id = 1;		
-		lOaN_Id = 1;		
+		catalog = new HashMap<>(); //Changed 'CaTaLoG' to 'catalog' 
+		members = new HashMap<>(); //Changed 'MeMbErS' to 'members'
+		loans = new HashMap<>(); //Changed 'LoAnS' to 'loans'
+		currentLoans = new HashMap<>(); //Changed 'CuRrEnT_LoAnS' to 'currentLoans'
+		damagedBooks = new HashMap<>(); //Changed 'DaMaGeD_BoOkS' to 'damagedBooks'
+		bookId = 1; //Changed 'bOoK_Id' to 'bookId'
+		memberId = 1; //Changed 'mEmBeR_Id' to 'memberId'	
+		loanId = 1; //Changed 'lOaN_Id;' to 'loadId'	
 	}
 
 	
-	public static synchronized Library GeTiNsTaNcE() {		
-		if (SeLf == null) {
-			Path PATH = Paths.get(lIbRaRyFiLe);			
-			if (Files.exists(PATH)) {	
-				try (ObjectInputStream LiBrArY_FiLe = new ObjectInputStream(new FileInputStream(lIbRaRyFiLe));) {
+	public static synchronized Library GetInstance() { // Chnages 'GeTiNsTaNcE()' to 'GetInstance()'		
+		if (self == null) { //Changed 'SeLf' to 'self'
+			Path path = Paths.get(LIBRARY_FILE); //Changed 'lIbRaRyFiLe' to 'LIBRARY_FILE' & 'PATH' to 'path'		
+			if (Files.exists(path)) { //Changed 'PATH' to 'path'	
+				try (ObjectInputStream libraryFile = new ObjectInputStream(new FileInputStream(LIBRARY_FILE));) { //Changed 'lIbRaRyFiLe' to 'LIBRARY_FILE' & 'LiBrArY_FiLe' to 'libraryFile'
 			    
-					SeLf = (Library) LiBrArY_FiLe.readObject();
-					Calendar.gEtInStAnCe().SeT_DaTe(SeLf.lOaN_DaTe);
-					LiBrArY_FiLe.close();
+					SeLf = (Library) libraryFile.readObject(); //Changed 'LiBrArY_FiLe' to 'libraryFile'
+					Calendar.getInstance().setDate(self.loanDate); //Changed 'gEtInStAnCe()' to 'getInstance' & 'SeT_DaTe' to 'setDate' & 'lOaN_DaTe' to 'loadDate' & 'SeLf' to 'self'
+					libraryFile.close(); //Changed 'LiBrArY_FiLe' to 'libraryFile'
 				}
 				catch (Exception e) {
 					throw new RuntimeException(e);
 				}
 			}
-			else SeLf = new Library();
+			else self = new Library();  //Changed 'SeLf' to 'self'
 		}
-		return SeLf;
+		return self;  //Changed 'SeLf' to 'self'
 	}
 
 	
-	public static synchronized void SaVe() {
-		if (SeLf != null) {
-			SeLf.lOaN_DaTe = Calendar.gEtInStAnCe().gEt_DaTe();
-			try (ObjectOutputStream LiBrArY_fIlE = new ObjectOutputStream(new FileOutputStream(lIbRaRyFiLe));) {
-				LiBrArY_fIlE.writeObject(SeLf);
-				LiBrArY_fIlE.flush();
-				LiBrArY_fIlE.close();	
+	public static synchronized void save() { //Changed 'SaVe' to 'save'
+		if (self != null) { //Changed 'SeLf' to 'self'
+			self.loanDate = Calendar.getInstance().getDate(); //Changed 'SeLf' to 'self' 7 'gEtInStAnCe()' to 'getInstance' & 'gEt_DaTe' to 'getDate' & 'lOaN_DaTe' to 'loadDate'
+			try (ObjectOutputStream libraryFile = new ObjectOutputStream(new FileOutputStream(LIBRARY_FILE));) { //Changed 'LiBrArY_FiLe' to 'libraryFile' & 'lIbRaRyFiLe' to 'LIBRARY_FILE'
+				libraryFile.writeObject(self); //Changed 'LiBrArY_FiLe' to 'libraryFile' & 'SeLf' to 'self'
+				libraryFile.flush(); //Changed 'LiBrArY_FiLe' to 'libraryFile'
+				libraryFile.close(); //Changed 'LiBrArY_FiLe' to 'libraryFile'
 			}
 			catch (Exception e) {
 				throw new RuntimeException(e);
@@ -84,28 +84,28 @@ public class Library implements Serializable {
 	}
 
 	
-	public int gEt_BoOkId() {
-		return bOoK_Id;
+	public int getBookId() { //Changed 'gEt_BoOkId' to 'getBookId'
+		return bookId; //Changed 'bOoK_Id' to 'bookId'
 	}
 	
 	
-	public int gEt_MeMbEr_Id() {
-		return mEmBeR_Id;
+	public int getMemberId() {//Changed 'gEt_MeMbEr_Id' to 'getMemberId'
+		return mEmBeR_Id; //Changed 'mEmBeR_Id' to 'memberId'
 	}
 	
 	
-	private int gEt_NeXt_BoOk_Id() {
-		return bOoK_Id++;
-	}
-
-	
-	private int gEt_NeXt_MeMbEr_Id() {
-		return mEmBeR_Id++;
+	private int getNextBookId() {//Changed 'gEt_NeXt_BoOk_Id' to 'getNextBookId'
+		return bookId++; //Changed 'bOoK_Id' to 'bookId'
 	}
 
 	
-	private int gEt_NeXt_LoAn_Id() {
-		return lOaN_Id++;
+	private int getNextMemberId() { //Changed 'gEt_NeXt_MeMbEr_Id' to 'getNextMemberId'
+		return mEmBeR_Id++; //Changed 'mEmBeR_Id' to 'memberId'
+	}
+
+	
+	private int getNextLoanId() { //Changed 'gEt_NeXt_LoAn_Id' to 'getNextLoadId'
+		return loanid++; //Changed 'lOaN_Id;' to 'loadId'
 	}
 
 	
